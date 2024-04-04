65cabd8dfb1ff11ad642955cbb042b02cc940f4b
rahulio96 <rahulgithub96@gmail.com>
2024-04-04 13:32:28 -0700

    Updated vs_texture_1

--- assets/shaders/vs_texture_1.glsl
+++ assets/shaders/vs_texture_1.glsl
@@ -2,16 +2,22 @@
 #version 430 core
 
 // TODO: add layout information here
+layout (location = 0) in vec3 aPosition;
+layout (location = 1) in vec4 aColor;
+layout (location = 2) in vec2 aTextureCoordinate;
 
 uniform mat4 uProjMatrix;
 uniform mat4 uViewMatrix;
 
 // TODO: declare outputs of vs shader here:
+out vec4 oColor;
+out vec2 oTC;
 
 
 void main()
 {
     // TODO:  define the two outputs here:
-
+    oColor = aColor;
+    oTC = aTextureCoordinate;
     gl_Position = uProjMatrix * uViewMatrix * vec4(aPos, 1.0);
 }
